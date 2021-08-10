b0aba6036a7cd68431a8a0266992b82d514732c6
Liliya Iskanderova <liliyaiskanderova@gmail.com>
2021-08-10 17:21:30 +0300

    HW2

--- src/test/java/scenarios/nativeMobileTests.java
+++ src/test/java/scenarios/nativeMobileTests.java
@@ -18,82 +18,82 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class nativeMobileTests extends BaseTest {
-//    @Test(groups = {"native"}, description = "Test checks an ability to register and log in with email and password",
-//            dataProvider = "registerAndLoginDataProvider", dataProviderClass = DataProvider.class)
-//    public void registerAndLoginWithEmailAndPasswordNativeTest(String email, String userName, String password,
-//                                                               String finalPageName)
-//            throws IllegalAccessException, NoSuchFieldException, InstantiationException {
-//        registerNewAccount(email, userName, password);
-//        System.out.println("Logging in");
-//        nativeLoginPageObject.getEmailField().sendKeys(email);
-//        nativeLoginPageObject.getPasswordField().sendKeys(password);
-//        nativeLoginPageObject.getSignInBtn().click();
-//        System.out.println("User logged in\nVerifying that user is on Budget Activity page");
-//        (new WebDriverWait(getDriver(), 3)).until(ExpectedConditions
-//                .refreshed(ExpectedConditions.visibilityOf(budgetActivityPageObject.getBudgetActivityTitle())));
-//        assertThat(budgetActivityPageObject.getBudgetActivityTitle().getText())
-//                .as("User is not on" + finalPageName + "page").isEqualTo(finalPageName);
-//        System.out.println("Page verified");
-//        navigateBackAndClearLoginPageFields();
-//        System.out.println("Test DONE");
-//    }
-//
-//    @Test(groups = {"native"}, description = "Test checks an ability to register and log in with username and password",
-//            dataProvider = "registerAndLoginDataProvider", dataProviderClass = DataProvider.class)
-//    public void registerAndLoginWithUsernameAndPasswordNativeTest(String email, String userName, String password,
-//                                                                  String finalPageName)
-//            throws IllegalAccessException, NoSuchFieldException, InstantiationException {
-//        registerNewAccount(email, userName, password);
-//        System.out.println("Logging in");
-//        nativeLoginPageObject.getEmailField().sendKeys(userName);
-//        nativeLoginPageObject.getPasswordField().sendKeys(password);
-//        nativeLoginPageObject.getSignInBtn().click();
-//        System.out.println("User logged in\nVerifying that user is on Budget Activity page");
-//        (new WebDriverWait(getDriver(), 3)).until(ExpectedConditions
-//                .refreshed(ExpectedConditions.visibilityOf(budgetActivityPageObject.getBudgetActivityTitle())));
-//        assertThat(budgetActivityPageObject.getBudgetActivityTitle().getText())
-//                .as("User is not on" + finalPageName + "page").isEqualTo(finalPageName);
-//        System.out.println("Page verified");
-//        navigateBackAndClearLoginPageFields();
-//        System.out.println("Test DONE");
-//    }
-
-    @Test(groups = {"native"}, description = "Test checks that user have not an ability to sign in without credentials",
-            dataProvider = "logInWithoutCredentialsDataProvider", dataProviderClass = DataProvider.class)
-    public void userCanNotSignInWithoutCredentialsNativeTest(String expectedMessage)
-            throws TesseractException, AWTException, IOException {
-        final String pathToScreenshot = "target/screenshots/screenshot.png";
-        System.out.println("Click Sign in button");
+    @Test(groups = {"native"}, description = "Test checks an ability to register and log in with email and password",
+            dataProvider = "registerAndLoginDataProvider", dataProviderClass = DataProvider.class)
+    public void registerAndLoginWithEmailAndPasswordNativeTest(String email, String userName, String password,
+                                                               String finalPageName)
+            throws IllegalAccessException, NoSuchFieldException, InstantiationException {
+        registerNewAccount(email, userName, password);
+        System.out.println("Logging in");
+        nativeLoginPageObject.getEmailField().sendKeys(email);
+        nativeLoginPageObject.getPasswordField().sendKeys(password);
         nativeLoginPageObject.getSignInBtn().click();
-        System.out.println("Sign in button clicked\nVerifying that message '" + expectedMessage + "' appears");
-        File file = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
-//        String filePath = "target/screenshots/screenshot.png";
-//        FileUtils.copyFile(file, new File(filePath));
-        System.out.println("Screenshot took");
-        Tesseract tesseract = new Tesseract();
-        tesseract.setDatapath("src/main/resources/tessdata");
-        tesseract.setLanguage("eng");
-        System.out.println("Doing OCR");
-        assertThat(tesseract.doOCR(file))
-                .as("'" + expectedMessage + "' was not found on the page")
-                .contains(expectedMessage);
-//        assertThat(tesseract.doOCR(new File(filePath)))
-//                .as("'" + expectedMessage + "' was not found on the page")
-//                .contains(expectedMessage);
-        System.out.println("Message verified\nTest DONE");
+        System.out.println("User logged in\nVerifying that user is on Budget Activity page");
+        (new WebDriverWait(getDriver(), 3)).until(ExpectedConditions
+                .refreshed(ExpectedConditions.visibilityOf(budgetActivityPageObject.getBudgetActivityTitle())));
+        assertThat(budgetActivityPageObject.getBudgetActivityTitle().getText())
+                .as("User is not on" + finalPageName + "page").isEqualTo(finalPageName);
+        System.out.println("Page verified");
+        navigateBackAndClearLoginPageFields();
+        System.out.println("Test DONE");
+    }
 
-//        Rectangle screenRectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-//        System.out.println("Screen size got");
-//        BufferedImage image = new Robot().createScreenCapture(screenRectangle);
+    @Test(groups = {"native"}, description = "Test checks an ability to register and log in with username and password",
+            dataProvider = "registerAndLoginDataProvider", dataProviderClass = DataProvider.class)
+    public void registerAndLoginWithUsernameAndPasswordNativeTest(String email, String userName, String password,
+                                                                  String finalPageName)
+            throws IllegalAccessException, NoSuchFieldException, InstantiationException {
+        registerNewAccount(email, userName, password);
+        System.out.println("Logging in");
+        nativeLoginPageObject.getEmailField().sendKeys(userName);
+        nativeLoginPageObject.getPasswordField().sendKeys(password);
+        nativeLoginPageObject.getSignInBtn().click();
+        System.out.println("User logged in\nVerifying that user is on Budget Activity page");
+        (new WebDriverWait(getDriver(), 3)).until(ExpectedConditions
+                .refreshed(ExpectedConditions.visibilityOf(budgetActivityPageObject.getBudgetActivityTitle())));
+        assertThat(budgetActivityPageObject.getBudgetActivityTitle().getText())
+                .as("User is not on" + finalPageName + "page").isEqualTo(finalPageName);
+        System.out.println("Page verified");
+        navigateBackAndClearLoginPageFields();
+        System.out.println("Test DONE");
+    }
+
+//    @Test(groups = {"native"}, description = "Test checks that user have not an ability to sign in without credentials",
+//            dataProvider = "logInWithoutCredentialsDataProvider", dataProviderClass = DataProvider.class)
+//    public void userCanNotSignInWithoutCredentialsNativeTest(String expectedMessage)
+//            throws TesseractException, AWTException, IOException {
+//        final String pathToScreenshot = "target/screenshots/screenshot.png";
+//        System.out.println("Click Sign in button");
+//        nativeLoginPageObject.getSignInBtn().click();
+//        System.out.println("Sign in button clicked\nVerifying that message '" + expectedMessage + "' appears");
+//        File file = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+////        String filePath = "target/screenshots/screenshot.png";
+////        FileUtils.copyFile(file, new File(filePath));
 //        System.out.println("Screenshot took");
 //        Tesseract tesseract = new Tesseract();
 //        tesseract.setDatapath("src/main/resources/tessdata");
 //        tesseract.setLanguage("eng");
 //        System.out.println("Doing OCR");
-//        assertThat(tesseract.doOCR(image))
+//        assertThat(tesseract.doOCR(file))
 //                .as("'" + expectedMessage + "' was not found on the page")
 //                .contains(expectedMessage);
+////        assertThat(tesseract.doOCR(new File(filePath)))
+////                .as("'" + expectedMessage + "' was not found on the page")
+////                .contains(expectedMessage);
 //        System.out.println("Message verified\nTest DONE");
+//
+////        Rectangle screenRectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+////        System.out.println("Screen size got");
+////        BufferedImage image = new Robot().createScreenCapture(screenRectangle);
+////        System.out.println("Screenshot took");
+////        Tesseract tesseract = new Tesseract();
+////        tesseract.setDatapath("src/main/resources/tessdata");
+////        tesseract.setLanguage("eng");
+////        System.out.println("Doing OCR");
+////        assertThat(tesseract.doOCR(image))
+////                .as("'" + expectedMessage + "' was not found on the page")
+////                .contains(expectedMessage);
+////        System.out.println("Message verified\nTest DONE");
     }
 
     private void registerNewAccount(String email, String userName, String password) {
